7f058c04bf02a36bcc05c6c77a40d5d6e85c276f
Irpan Budiana <irpan.budiana@gmail.com>
2020-11-29 00:00:35 +0700

    Templating Form

--- application/views/form_add.php
+++ application/views/form_add.php
@@ -1,28 +1,40 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <h1>Tambah Artikel</h1>
+<?php $this->load->view('header.php'); ?>
+
+<header class="masthead" style="background-image: url('<?php echo base_url();?>assets/img/about-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-heading">
+                <h1>Tambah Artikel</h1>
+            </div>
+        </div>
+      </div>
+    </div>
+</header>
+
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="com-md-8">
+      <h1>Tambah Artikel</h1>
         
-        <form method="POST">
-            <div>
-                <label>Judul</div>
-                <input type="text" name="title">
-            </div>
+      <form method="POST">
+        <div>
+            <label>Judul</label>
+            <input type="text" class="form-control" name="title">
+        </div>
 
-            <div>
-                <label>URL</div>
-                <input type="text" name="url">
-            </div>
+        <div>
+            <label>URL</label>
+            <input type="text"  class="form-control" name="url">
 
-            <div>
-                <label>Konten</div>
-                <textarea cols="30" rows="10" name="content"></textarea>
-            </div>
+        </div> 
+        
+        <div>
+            <label>Konten</label>
+            <textarea name="content"  cols="30" rows="10" class="form-control" id=""></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan Artikel</button>
+    </form>
 
-            <button type="submit">Simpan Artikel</button>
-        </form>
-    </body>
-</html>
+<?php $this->load->view('footer.php'); ?>
