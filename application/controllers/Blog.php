daa4b96b443aeb57b1c138140edf6d838dce743a
Irpan Budiana <irpan.budiana@gmail.com>
2020-11-28 10:16:37 +0700

    halaman awal blog dan default controller

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -2,11 +2,23 @@
 
 Class Blog extends CI_Controller 
 {
-    public function index($nama, $goldarah, $alamat) {
+    public function index() {
 
-        $data['nama']       = $nama;
-        $data['goldarah']   = $goldarah;
-        $data['alamat']     = $alamat;
+        $data['blogs']=[
+            ['title'=>'Artikel Pertama',
+            'content'=>'<p>Lorem Ipsum is simply dummy text of the printing and     
+             typesetting industry.</p>'],
+            ['title'=>'Artikel Kedua',
+            'content'=>'<p>Lorem Ipsum has been the industrys standard dummy 
+             text ever since the 1500s, when an unknown printer took a galley of type 
+             and scrambled it to make a type specimen book.</p>'],
+            ['title'=>'Artikel Ketiga',
+            'content'=>'<p>Lorem Ipsum is simply dummy text of the printing and     
+             typesetting industry. Lorem Ipsum has been the industrys standard dummy 
+             text ever since the 1500s.</p>']
+            ];
+
+        
         $this->load->view("blog", $data);
     }
 
