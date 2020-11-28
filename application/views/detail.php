a783453c14ca255d8b191e400be6f382bd491a84
Irpan Budiana <irpan.budiana@gmail.com>
2020-11-28 21:40:33 +0700

    template detail artikel

--- application/views/detail.php
+++ application/views/detail.php
@@ -1,12 +1,31 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Detail Blog</h1>
-    <div class="blog">
-        <h2><?php echo $blogs['title'];?></h2>
-        <?php echo $blogs['content'];   ?>  
-</body>
-</html>
+<?php $this->load->view('header.php') ?>
+
+<!-- Page Header -->
+<header class="masthead" style="background-image: url('img/post-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="post-heading">
+            <h1><?php echo $blogs['title']?></h1>
+            <span class="meta">Posted on <?php echo $blogs['date'];?></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Post Content -->
+  <article>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <?php echo $blogs['content'];?>
+        </div>
+      </div>
+    </div>
+  </article>
+
+  <hr>
+
+  <?php $this->load->view('footer.php') ?>
