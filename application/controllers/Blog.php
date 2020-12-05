c6b380e2dbc724c18337361a0e98404a6ad3d525
Irpan Budiana <irpan.budiana@gmail.com>
2020-12-05 11:06:33 +0700

    validasi form tambah artikel

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -21,34 +21,41 @@ Class Blog extends CI_Controller
 
     public function add() {
         if ($this->input->post()) {
-            $data['title'] = $this->input->post('title'); //$_POST['title']
-            $data['url']   = $this->input->post('url');
-            $data['content']=$this->input->post('content');
 
-            // konfigurasi upload
-            $config['upload_path']   = './uploads/';
-            $config['allowed_types'] = 'gif|png|jpg';
-            $config['max_size']   = 1000;
-            $config['max_width']  = 2000;
-            $config['max_height'] = 1600;
+            $this->form_validation->set_rules('title', 'Judul', 'required');
+            $this->form_validation->set_rules('url', 'URL', 'required|alpha_dash');
+            $this->form_validation->set_rules('content', 'Konten', 'required');
 
-            $this->load->library('upload', $config);
+            if ($this->form_validation->run()===TRUE) {
+                $data['title'] = $this->input->post('title'); //$_POST['title']
+                $data['url']   = $this->input->post('url');
+                $data['content']=$this->input->post('content');
 
-            if (!$this->upload->do_upload('cover')) {
-                echo $this->upload->display_errors();
-            } else {
-                $data['cover'] = $this->upload->data('file_name');
-            }
-    
-            $id = $this->Blog_model->insertBlog($data);
-            if ($id) {
-                echo "Data berhasil disimpan";
-                redirect('/');
-            }
-            else    
-                echo "Data gagal disimpan";
+                // konfigurasi upload
+                $config['upload_path']   = './uploads/';
+                $config['allowed_types'] = 'gif|png|jpg';
+                $config['max_size']   = 1000;
+                $config['max_width']  = 2000;
+                $config['max_height'] = 1600;
+
+                $this->load->library('upload', $config);
+
+                if (!$this->upload->do_upload('cover')) {
+                    echo $this->upload->display_errors();
+                } else {
+                    $data['cover'] = $this->upload->data('file_name');
+                }
+        
+                $id = $this->Blog_model->insertBlog($data);
+                if ($id) {
+                    echo "Data berhasil disimpan";
+                    redirect('/');
+                }
+                else {    
+                    echo "Data gagal disimpan";
+                }
+            }            
         }
-
         $this->load->view('form_add');
     }
 
--- application/views/form_add.php
+++ application/views/form_add.php
@@ -17,27 +17,31 @@
   <div class="row justify-content-center">
     <div class="com-md-8">
       <h1>Tambah Artikel</h1>
+
+      <div class="alert alert-warning">
+        <?php echo validation_errors(); ?>
+      </div>
         
       <?php echo form_open_multipart(); ?>
       
       <div class="form-group">
             <label>Judul</label>
-            <?php echo form_input('title', null, 'class="form-control"'); ?>
+            <?php echo form_input('title', set_value('title'), 'class="form-control"'); ?>
       </div>
 
       <div class="form-group">
             <label>URL</label>
-            <?php echo form_input('url', null, 'class="form-control"'); ?>
+            <?php echo form_input('url', set_value('url'), 'class="form-control"'); ?>
       </div>
 
       <div class="form-group">
             <label>Konten</label>
-            <?php echo form_textarea('content', null, 'class="form-control" id="" cols="30" rows="10"'); ?>
+            <?php echo form_textarea('content', set_value('content'), 'class="form-control" id="" cols="30" rows="10"'); ?>
       </div>
 
       <div class="form-group">
           <label>Cover</label>
-          <?php echo form_upload('cover', null, 'class="form-control"'); ?>
+          <?php echo form_upload('cover', set_value('cover'), 'class="form-control"'); ?>
       </div>
       <br>
       <button type="submit" class="btn btn-primary">Simpan Artikel</button>
